acb19f6f2df2d71cf272c52c285cc4f50755a2c6
ch0p5u3y <ch0p5u3y@mail.com>
2019-09-23 08:42:17 +0100

    added new writeups

---
+++ _site/about/barchart.js
@@ -0,0 +1,33 @@
+var ctx = document.getElementById("graph");
+    var data = {
+	            labels: "C, C++, Assembly Language, Python, Java".split(", "),
+	            datasets: [{
+			                label: "Ability",
+			                data: parse("15, 20, 10, 30, 25"),
+			                backgroundColor: parseColor("1, 3, 10, 9, 6")
+			            }]
+	        };
+    var __my_ch4rt__ = new Chart(ctx, {
+	            type: 'polarArea',
+	            data: data,
+	            options: {
+			                responsive: true,
+			                legend: {display: false},
+			                animation: {
+						                animateScale: true, animateRotate: true
+						            }
+			            }
+	        });
+    function parse(x) {
+	            return x.split(", ").map(function(e) {
+			                return parseInt(e);
+			            });
+	        }
+    function parseColor(s) {
+	            var colorMap = [
+			                'rgb(36,74,225)','rgb(255,212,36)','rgb(93,225,36)','rgb(225,137,36)','rgb(225,36,36)','rgb(124,36,225)','rgb(36,181,225)','rgb(130,225,36)','rgb(212,225,36)','rgb(225,36,105)','rgb(134,132,128)'
+			            ];
+	            return s.split(", ").map(function(e) {
+			                return colorMap[parseInt(e)];
+			            });
+	        }
